a02acd0de489f2bfc1a0b9a6664deb8a907994c5
axton21 <axton_n@yahoo.com>
2015-05-04 14:41:14 -0400

    done

--- spec/greeting.spec.js
+++ spec/greeting.spec.js
@@ -5,11 +5,11 @@ describe('greeting', function() {
     expect(greeting(6)).toEqual('Good Morning!');
   });
 
-  it('returns Good Afternoon! during the morning', function() {
+  it('returns Good Afternoon! during the afternoon', function() {
     expect(greeting(15)).toEqual('Good Afternoon!');
   });
 
-  it('returns Good Night! during the morning', function() {
+  it('returns Good Night! during the night', function() {
     expect(greeting(23)).toEqual('Good Night!');
   });
 });
